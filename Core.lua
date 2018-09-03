49477b924641125cbddf9509d66b8cd0a7d79776
moody <dethanyel@gmail.com>
2018-09-03 02:37:49 -0400

    various fixes and improvements

--- Core.lua
+++ Core.lua
@@ -5,18 +5,20 @@ local HBD = LibStub("HereBeDragons-2.0")
 local HBDPins = LibStub("HereBeDragons-Pins-2.0")
 
 -- Upvalues
-local floor, format, ipairs, pairs, print, tconcat, tremove =
-      floor, format, ipairs, pairs, print, table.concat, table.remove
+local format, pairs, print, tconcat = format, pairs, print, table.concat
 
-local sqrt = math.sqrt
-
-local WorldMapTooltip = WorldMapTooltip
+local C_Map, C_TaxiMap, Enum = C_Map, C_TaxiMap, Enum
+local WorldMapFrame, WorldMapTooltip = WorldMapFrame, WorldMapTooltip
+local FlightPointDataProviderMixin = FlightPointDataProviderMixin
+local IsIndoors, UnitFactionGroup = IsIndoors, UnitFactionGroup
 
 -- Modules
 local Core = DethsLibLoader("DethsAddonLib", "1.0"):Create(AddonName)
-Core.Nearest = {} -- Array of nearest flight master names
 
 -- Consts
+local PLAYER_FACTION_GROUP = nil
+local TAXI_NODES = {}
+
 local WHISTLE_MAPS = {
   -- uiMapIDs for calls to C_TaxiMap.GetTaxiNodesForMap()
   [630] = true, -- Azsuna (Broken Isles)
@@ -25,9 +27,10 @@ local WHISTLE_MAPS = {
   [882] = true, -- Mac'Aree (Argus)
   [895] = true, -- Tiragarde Sound (Kul'tiras)
   [862] = true, -- Zuldazar (Zandalar)
+}
 
-  -- [[ Debug ]] --
-  -- [63] = true, -- Ashenvale
+local WHISTLE_MAPS_IGNORE = {
+  [627] = true, -- Dalaran
 }
 
 local WHISTLE_CONTINENTS = {
@@ -36,40 +39,33 @@ local WHISTLE_CONTINENTS = {
   [905] = true, -- Argus
   [876] = true, -- Kul'tiras
   [875] = true, -- Zandalar
-
-  -- [[ Debug ]]
-  -- [12] = true, -- Kalimdor
 }
 
-local TAXI_NODES = {}
-
 -- Variables
-local currentMapID = -1
-local currentZoneMapID = -1
-local currentMapName = ""
+local currentMapID = -1     -- map where the player is, such as Boralus
+local currentZoneMapID = -1 -- map of the actual zone, such as Tiragarde Sound
+local currentZoneMapName = ""
 local canUseWhistle = false
+local pinsNeedUpdate = false
+local nearestTaxis = {}
+
+-- ============================================================================
+-- Helper Functions
+-- ============================================================================
+
+local function whistleCanBeUsed()
+  return canUseWhistle and not IsIndoors()
+end
+
+-- Debug
+local function debug(...) print(format("|cFF98FB98[%s]|r", AddonName, ...)) end
 
 -- ============================================================================
 -- DAL Functions
 -- ============================================================================
 
 function Core:OnInitialize()
-  self:Print("Loaded!")
-
-  -- -- Initialize Taxi data
-  -- local factionGroup = UnitFactionGroup("PLAYER")
-  -- local FlightPointDataProviderMixin = FlightPointDataProviderMixin
-  -- for mapID in pairs(WHISTLE_MAPS) do
-  --   local taxiNodes = C_TaxiMap.GetTaxiNodesForMap(mapID)
-  --   print(format("%d taxis in %d", #taxiNodes, mapID))
-  --   for i, taxiNodeInfo in ipairs(taxiNodes) do
-  --     if FlightPointDataProviderMixin:ShouldShowTaxiNode(factionGroup, taxiNodeInfo) then
-  --       TAXI_NODES[#TAXI_NODES+1] = taxiNodeInfo
-  --     end
-  --   end
-  -- end
-
-  -- print("Total Taxis:", "#"..#TAXI_NODES)
+  PLAYER_FACTION_GROUP = UnitFactionGroup("PLAYER")
 end
 
 do -- OnUpdate()
@@ -77,50 +73,45 @@ do -- OnUpdate()
   local timer = DELAY
 
   function Core:OnUpdate(elapsed)
-    if not canUseWhistle then return end
     timer = timer + elapsed
     if (timer >= DELAY) then
-      self:UpdateNearest()
+      self:UpdateTaxis()
       timer = 0
     end
+    self:UpdatePins()
   end
 
   -- HBD Callback
   HBD.RegisterCallback(AddonName, "PlayerZoneChanged", function(_, mapID)
-    Core:Print("HBD PlayerZoneChanged, MapID = "..tostring(mapID))
     currentMapID = mapID
     currentZoneMapID = mapID
 
     local mapInfo = C_Map.GetMapInfo(mapID or 0)
     if mapInfo and mapInfo.name then
-      timer = DELAY
-      currentMapName = mapInfo.name
+      currentZoneMapName = mapInfo.name
 
       -- Update data based on current zone and continent
       while mapInfo.mapType and (mapInfo.mapType > Enum.UIMapType.Continent) do
         if (mapInfo.mapType == Enum.UIMapType.Zone) then
-          -- print("Map name:", mapInfo.name)
-          currentMapName = mapInfo.name
+          currentZoneMapName = mapInfo.name
           currentZoneMapID = mapInfo.mapID
         end
         mapInfo = C_Map.GetMapInfo(mapInfo.parentMapID)
       end
 
       if (mapInfo.mapType == Enum.UIMapType.Continent) then
-        canUseWhistle = WHISTLE_CONTINENTS[mapInfo.mapID]
-        -- print("Can use whistle? ", tostring(canUseWhistle))
-        
+        canUseWhistle = WHISTLE_CONTINENTS[mapInfo.mapID] and not WHISTLE_MAPS_IGNORE[currentMapID]
+        if not canUseWhistle then return end
         -- Update taxi data for current map
-        local factionGroup = UnitFactionGroup("PLAYER")
-        local FlightPointDataProviderMixin = FlightPointDataProviderMixin
         local taxiNodes = C_TaxiMap.GetTaxiNodesForMap(mapID)
-        -- print(format("%d taxis", #taxiNodes, mapID))
-        for i, taxiNodeInfo in ipairs(taxiNodes) do
-          if FlightPointDataProviderMixin:ShouldShowTaxiNode(factionGroup, taxiNodeInfo) then
+        for _, taxiNodeInfo in pairs(taxiNodes) do
+          if FlightPointDataProviderMixin:ShouldShowTaxiNode(PLAYER_FACTION_GROUP, taxiNodeInfo) then
             TAXI_NODES[taxiNodeInfo.name] = taxiNodeInfo
           end
         end
       end
+
+      timer = DELAY -- update immediately
     end
   end)
 end
@@ -150,12 +141,14 @@ local getPin, clearPins do
     WorldMapTooltip:Hide()
   end
 
-  getPin = function()
-    local pin = tremove(pool)
+  getPin = function(name)
+    local pin = next(pool)
     
-    if not pin then
+    if pin then
+      pool[pin] = nil
+    else
       count = count + 1
-      pin = CreateFrame("Button", AddonName.."Pin"..count)
+      pin = CreateFrame("Button", AddonName.."Pin"..count, WorldMapFrame)
       pin:SetSize(20, 20)
 
       pin.texture = pin:CreateTexture(AddonName.."PinTexture"..count, "BACKGROUND")
@@ -174,6 +167,7 @@ local getPin, clearPins do
       pins[#pins+1] = pin
     end
 
+    pin.name = name
     pin:Show()
     return pin
   end
@@ -181,8 +175,9 @@ local getPin, clearPins do
   clearPins = function()
     for _, pin in pairs(pins) do
       pin:Hide()
-      pool[#pool+1] = pin
+      pool[pin] = true
     end
+    HBDPins:RemoveAllWorldMapIcons(Addon)
   end
 end
 
@@ -190,25 +185,18 @@ end
 -- General Functions
 -- ============================================================================
 
-do -- Print()
-  local title = format("|cFF98FB98[%s]|r ", AddonName)
-  function Core:Print(msg) print(title..msg) end
-end
-
-do -- UpdateNearest()
-  local TAXI_NAME_PATTERN = "(.+),"
-  local THRESHOLD = 0.005
-  
+do -- UpdateTaxis()
+  local THRESHOLD = 100 * 0.5 -- yards
   local lastX, lastY = -1, -1
   local zoneTaxis = {}
-  local nearestTaxis = {}
 
-  local function getDistance(x, y, x2, y2)
-    local dx, dy = x - x2, y - y2
-    return sqrt(dx * dx + dy * dy)
-  end
+  function Core:UpdateTaxis()
+    if not whistleCanBeUsed() then
+      for k in pairs(zoneTaxis) do zoneTaxis[k] = nil end
+      for k in pairs(nearestTaxis) do nearestTaxis[k] = nil end
+      return
+    end
 
-  function Core:UpdateNearest()
     local x, y, mapID = HBD:GetPlayerZonePosition()
     if not x then return end
 
@@ -218,55 +206,50 @@ do -- UpdateNearest()
     lastY = y
 
     -- Clear data
-    clearPins()
-    HBDPins:RemoveAllWorldMapIcons(Addon)
-
-    for k in pairs(self.Nearest) do self.Nearest[k] = nil end
     for k in pairs(zoneTaxis) do zoneTaxis[k] = nil end
     for k in pairs(nearestTaxis) do nearestTaxis[k] = nil end
 
     -- Get taxis in current zone
     for _, taxi in pairs(TAXI_NODES) do
-      if (taxi.name:find(currentMapName, 1, true)) then
+      if (taxi.name:find(currentZoneMapName, 1, true)) then
         zoneTaxis[#zoneTaxis+1] = taxi
       end
     end
 
+    -- If no taxis, return
     if (#zoneTaxis == 0) then return end
 
     -- Calculate nearest taxis
     local currentNearest = zoneTaxis[1]
     local currentDistance = HBD:GetZoneDistance(currentMapID, x, y, currentZoneMapID, currentNearest.position.x, currentNearest.position.y)
-    -- print("currentDistance", currentDistance)
-    self.Nearest[1] = currentNearest.name:match(TAXI_NAME_PATTERN)
     nearestTaxis[1] = currentNearest
 
     for i=2, #zoneTaxis do
       local taxi = zoneTaxis[i]
       local distance = HBD:GetZoneDistance(currentMapID, x, y, currentZoneMapID, taxi.position.x, taxi.position.y)
-      -- print("distance", distance)
       -- If closer, wipe nearests and set to nearest
       if (distance < (currentDistance - THRESHOLD)) then -- wipe and add
-        -- print("set current to", taxi.name)
-        for k in pairs(self.Nearest) do self.Nearest[k] = nil end
         for k in pairs(nearestTaxis) do nearestTaxis[k] = nil end
-        self.Nearest[#self.Nearest+1] = taxi.name:match(TAXI_NAME_PATTERN)
         nearestTaxis[#nearestTaxis+1] = taxi
         currentDistance = distance
       elseif (distance < (currentDistance + THRESHOLD)) then -- add
-        self.Nearest[#self.Nearest+1] = taxi.name:match(TAXI_NAME_PATTERN)
         nearestTaxis[#nearestTaxis+1] = taxi
       end
     end
 
-    -- print("#nearestTaxis", #nearestTaxis)
+    clearPins()
+    pinsNeedUpdate = true
+  end
+end
 
-    -- Add pins to map
-    for _, taxi in pairs(nearestTaxis) do
-      local pin = getPin()
-      pin.name = taxi.name
-      HBDPins:AddWorldMapIconMap(Addon, pin, currentMapID, taxi.position.x, taxi.position.y)
-    end
+function Core:UpdatePins()
+  if not whistleCanBeUsed() then clearPins() return end
+  if not WorldMapFrame:IsVisible() or not pinsNeedUpdate then return end
+  pinsNeedUpdate = false
+
+  -- Add pins to map
+  for _, taxi in pairs(nearestTaxis) do
+    HBDPins:AddWorldMapIconMap(Addon, getPin(taxi.name), currentMapID, taxi.position.x, taxi.position.y)
   end
 end
 
@@ -275,21 +258,31 @@ end
 -- ============================================================================
 
 do
-  -- local FMW_ID = "774" -- Malachite ID, for debugging
-  FMW_ID = "141605" -- Flight Master's Whistle Item ID
+  local FMW_ID = "141605" -- Flight Master's Whistle Item ID
   local LEFT = format("|cFF98FB98%s:|r", AddonName)
   local buffer = {}
 
-  local function concat(t)
+  local function sortFunc(a, b) return a.name < b.name end
+
+  -- "Tradewinds Market, Tiragarde Sound" -> "Tradewinds Market"
+  local function getTaxiName(taxi)
+    local name = taxi.name:match("(.+),")
+    return name or taxi.name
+  end
+
+  local function getTaxiNames()
+    if (#nearestTaxis == 1) then return getTaxiName(nearestTaxis[1]) end
+    table.sort(nearestTaxis, sortFunc)
     for k in pairs(buffer) do buffer[k] = nil end
-    if (#t == 1) then return t[1] end
-    for i=1, (#t - 1) do buffer[#buffer+1] = t[i]..", " end
-    buffer[#buffer+1] = t[#t]
+    for i=1, (#nearestTaxis - 1) do
+      buffer[#buffer+1] = getTaxiName(nearestTaxis[i])..", "
+    end
+    buffer[#buffer+1] = getTaxiName(nearestTaxis[#nearestTaxis])
     return tconcat(buffer)
   end
 
   local function onTooltipSetItem(self)
-    if not canUseWhistle or (#Core.Nearest == 0) then return end
+    if not whistleCanBeUsed() or (#nearestTaxis == 0) then return end
 
     -- Verify the item is the Flight Master's Whistle
     local link = select(2, self:GetItem())
@@ -298,7 +291,7 @@ do
     if not id or (id ~= FMW_ID) then return end
     
     self:AddLine(" ") -- Blank Line
-    self:AddDoubleLine(LEFT, concat(Core.Nearest), nil, nil, nil, 1, 1, 1)
+    self:AddDoubleLine(LEFT, getTaxiNames(), nil, nil, nil, 1, 1, 1)
   end
 
   GameTooltip:HookScript("OnTooltipSetItem", onTooltipSetItem)
